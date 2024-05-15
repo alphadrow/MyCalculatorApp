2707e68c3aa86186c88f08c230d7c2a9775d10a6
User <user@MacBook-Air-User.local>
2024-05-15 14:04:06 +0300

    Написал обработку каждой кнопки из макета. Заглушки методов в бизнес логике Калькулятора

--- app/src/main/java/ru/alphadrow/gb/mycalculatorapp/Calculator.java
+++ app/src/main/java/ru/alphadrow/gb/mycalculatorapp/Calculator.java
@@ -1,2 +1,23 @@
-package ru.alphadrow.gb.mycalculatorapp;public class Calculator {
+package ru.alphadrow.gb.mycalculatorapp;
+public class Calculator {
+    NumberBuilder numberBuilder = new NumberBuilder();
+
+    public void plus() {
+    }
+
+    public void minus() {
+    }
+
+    public void multiply() {
+    }
+
+    public void divide() {
+    }
+
+    public void myEquals() {
+    }
+
+    public void append(int i) {
+        numberBuilder.append(i);
+    }
 }
